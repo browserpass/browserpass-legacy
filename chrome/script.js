f767e75dd6246c84e1867164c38b9bdaf05ee87a
Danny van Kooten <dannyvankooten@gmail.com>
2016-11-04 15:26:17 +0100

    communicate with binary through native messaging instead of local http server.

--- chrome/script.js
+++ chrome/script.js
@@ -1,36 +1,77 @@
+'use strict';
+
+var app = 'com.dannyvankooten.gopass';
+var content = document.getElementById('content');
+var domain = '';
+var favicon = '';
+
+content.innerHTML = '<span class="loader"></span>';
 chrome.browserAction.setIcon({ path: 'icon.svg' });
 chrome.tabs.getSelected(null, init);
 
 function init(tab) {
-  var domain = parseDomainFromUrl(tab.url);
-
-  fetch("http://localhost:3131/password?domain="+domain)
-    .then(function(response) { return response.json(); })
-    .then(function(results) {
-        // if just 1 result, fill & close
-        if(results.length === 1 ) {
-          fill(results[0]);
-          window.close();
-        }
-
-        // if multiple: offer choice
-
-        // if none: show sad thing
-    });
+  favicon = tab.favIconUrl;
+  domain = parseDomainFromUrl(tab.url);
+  chrome.runtime.sendNativeMessage(app, { "domain": domain }, handleResponse);
 }
 
-function fill(creds) {
-  var code = '';
-  code += 'document.querySelector(\'input[type=\"text\"], input[type=\"email\"]\').value = "'+ creds.u +'";';
-  code += 'document.querySelector(\'input[type=\"password\"]\').value = "'+ creds.p +'";';
+// handle response received from native binary
+function handleResponse(response) {
+  content.innerHTML = '';
 
-  chrome.tabs.executeScript({
-    code: code
-  })
+  // check for communication error
+  if( response === undefined ) {
+    content.innerHTML = '<div class="status-text">Error talking to pass.</div>';
+    return;
+  }
+
+  if( response.length === 0 ) {
+    // no results
+    content.innerHTML = '<div class="status-text">No passwords found for <strong>' + domain + "</strong>.</div>";
+    return;
+  }
+
+// if just 1, fill right away
+  if( response.length === 1 ) {
+    return fillLoginForm.call(response[0]);
+  }
+
+  var list = document.createElement('div');
+  list.className = 'usernames';
+  content.appendChild(list);
+
+  for( var i=0; i<response.length; i++ ) {
+    var el = document.createElement('div');
+    el.className = 'username';
+    el.onclick = fillLoginForm.bind(response[i]);
+
+    var html = '';
+    html += '<img class="favicon" src="'+ favicon +'" />';
+    html += '<span>' + response[i].u + '</span>';
+    el.innerHTML = html;
+    list.appendChild(el);
+  }
 }
 
+// fill login form & submit
+function fillLoginForm() {
+  var code = 'console.log("Filling login form.");';
+  code += 'var passwordInput = document.querySelector(\'input[type="password"]\');';
+  code += 'var origForm = passwordInput.form; var newForm = origForm.cloneNode(true);';
+  code += 'var passwordInput = newForm.querySelector(\'input[type="password"]\');'
+  code += "var usernameInput = newForm.querySelector('input[type=email], input[type=text]');";
+  code += 'passwordInput.value = '+ JSON.stringify(this.p) +';';
+  code += 'usernameInput.value = '+ JSON.stringify(this.u) +';';
+  code += 'origForm.parentNode.replaceChild(newForm, origForm);';
+  code += 'newForm.submit();';
+
+  chrome.tabs.executeScript({ code: code });
+  window.close();
+}
+
+// parse domain from a URL & strip WWW
 function parseDomainFromUrl(url) {
   var a = document.createElement('a');
   a.href = url;
-  return a.hostname;
+  return a.hostname.replace('www.', '');
 }
