0bb27248dc00b4c52cf157b213cfa2745297973c
Danny van Kooten <dannyvankooten@gmail.com>
2016-11-04 16:45:12 +0100

    resolve binary filename in install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,18 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 HOST_NAME=com.dannyvankooten.gopass
+HOST_FILE=$DIR/gopass
 
 # Find target dir: see https://developer.chrome.com/extensions/nativeMessaging#native-messaging-host-location
 if [ $(uname -s) == 'Darwin' ]; then
+  BINARY_FILE=$DIR/gopass-darwinx64
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
+  BINARY_FILE=$DIR/gopass-linux64
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
@@ -27,9 +30,8 @@ mkdir -p "$TARGET_DIR"
 cp "$DIR/$HOST_NAME.json" "$TARGET_DIR"
 
 # Update host path in the manifest.
-HOST_PATH=$DIR/gopass
-ESCAPED_HOST_PATH=${HOST_PATH////\\/}
-sed -i -e "s/%%replace%%/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
+ESCAPED_HOST_FILE=${HOST_FILE////\\/}
+sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR/$HOST_NAME.json"
 
 # Set permissions for the manifest so that all users can read it.
 chmod o+r "$TARGET_DIR/$HOST_NAME.json"
