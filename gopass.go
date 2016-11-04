bfee861ea78ab29c4adecbf39a765a4f71e72883
Danny van Kooten <dannyvankooten@gmail.com>
2016-11-04 15:25:59 +0100

    rewrite go binary to communicate through stdio & native messaging

--- gopass.go
+++ gopass.go
@@ -3,10 +3,9 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,35 +13,65 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/password", getPasswordHandler)
-	log.Fatal(http.ListenAndServe("localhost:3131", nil))
+	// set logging
+	f, err := os.OpenFile("debug.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	checkError(err)
+	defer f.Close()
+	log.SetOutput(f)
+
+	// listen for stdin
+	for {
+		// get message length, 4 bytes
+		var data map[string]string
+		var length uint32
+		err := binary.Read(os.Stdin, binary.LittleEndian, &length)
+		if err != nil {
+			break
+		}
+
+		input := make([]byte, length)
+		_, err = os.Stdin.Read(input)
+		if err != nil {
+			break
+		}
+
+		err = json.Unmarshal(input, &data)
+		checkError(err)
+
+		usernames := getUsernames(data["domain"])
+		results := make([]map[string]string, 0)
+
+		for _, username := range usernames {
+			password, _ := getPassword(data["domain"], username)
+			results = append(results, map[string]string{
+				"u": username,
+				"p": password,
+			})
+		}
+
+		jsonResponse, err := json.Marshal(results)
+		checkError(err)
+		binary.Write(os.Stdout, binary.LittleEndian, uint32(len(jsonResponse)))
+		_, err = os.Stdout.Write(jsonResponse)
+		checkError(err)
+	}
 }
 
-func getPasswordHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s: %s\n", r.Method, r.URL.RequestURI())
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	domain := r.URL.Query().Get("domain")
+// get list of usernames for the domain
+func getUsernames(domain string) []string {
 	matches, _ := filepath.Glob(os.Getenv("HOME") + "/.password-store/" + domain + "/*.gpg")
-	results := make([]map[string]string, 0)
+	usernames := make([]string, 0)
 
 	for _, file := range matches {
 		_, filename := filepath.Split(file)
 		username := strings.TrimSuffix(filename, filepath.Ext(filename))
-		password, err := getPassword(domain, username)
-		checkError(err)
-		results = append(results, map[string]string{
-			"u": username,
-			"p": password,
-		})
+		usernames = append(usernames, username)
 	}
 
-	json.NewEncoder(w).Encode(results)
+	return usernames
 }
 
-// run pass to get decrypted file content
+// runs pass to get decrypted file content
 func getPassword(domain string, username string) (string, error) {
 	var out bytes.Buffer
 	cmd := exec.Command("pass", domain+"/"+username)
@@ -52,7 +81,7 @@ func getPassword(domain string, username string) (string, error) {
 		return "", err
 	}
 
-	// read first line (password)
+	// read first line (the password)
 	scanner := bufio.NewScanner(&out)
 	scanner.Scan()
 	password := scanner.Text()
