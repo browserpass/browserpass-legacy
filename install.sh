9a313be3ad53ecdfb4bc53dd1c2543af5570038f
Danny van Kooten <dannyvankooten@gmail.com>
2017-02-10 10:57:29 +0100

    add support for vivaldi, closes #36

--- install.sh
+++ install.sh
@@ -15,10 +15,12 @@ if [ $(uname -s) == 'Darwin' ]; then
     TARGET_DIR_CHROME="/Library/Google/Chrome/NativeMessagingHosts"
     TARGET_DIR_CHROMIUM="/Library/Application Support/Chromium/NativeMessagingHosts"
     TARGET_DIR_FIREFOX="/Library/Application Support/Mozilla/NativeMessagingHosts"
+    TARGET_DIR_VIVALDI="/Library/Application Support/Vivaldi/NativeMessagingHosts"
   else
     TARGET_DIR_CHROME="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
     TARGET_DIR_CHROMIUM="$HOME/Library/Application Support/Chromium/NativeMessagingHosts"
     TARGET_DIR_FIREFOX="$HOME/Library/Application Support/Mozilla/NativeMessagingHosts"
+    TARGET_DIR_VIVALDI="$HOME/Library/Application Support/Vivaldi/NativeMessagingHosts"
   fi
 else
   HOST_FILE="$DIR/browserpass-linux64"
@@ -26,10 +28,12 @@ else
     TARGET_DIR_CHROME="/etc/opt/chrome/native-messaging-hosts"
     TARGET_DIR_CHROMIUM="/etc/chromium/native-messaging-hosts"
     TARGET_DIR_FIREFOX="/usr/lib/mozilla/native-messaging-hosts"
+    TARGET_DIR_VIVALDI="/etc/chromium/native-messaging-hosts"
   else
     TARGET_DIR_CHROME="$HOME/.config/google-chrome/NativeMessagingHosts"
-    TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts/"
+    TARGET_DIR_CHROMIUM="$HOME/.config/chromium/NativeMessagingHosts"
     TARGET_DIR_FIREFOX="$HOME/.mozilla/native-messaging-hosts"
+    TARGET_DIR_VIVALDI="$HOME/.config/vivaldi/NativeMessagingHosts"
   fi
 fi
 
@@ -47,33 +51,43 @@ echo "===================="
 echo "1) Chrome"
 echo "2) Chromium"
 echo "3) Firefox"
-echo -n "1-3: "
+echo "4) Vivaldi"
+echo -n "1-4: "
 read BROWSER
 echo ""
 
 # Set target dir from user input
 if [[ "$BROWSER" == "1" ]]; then
+  BROWSER_NAME="Chrome"
   TARGET_DIR="$TARGET_DIR_CHROME"
 fi
 
 if [[ "$BROWSER" == "2" ]]; then
+  BROWSER_NAME="Chromium"
   TARGET_DIR="$TARGET_DIR_CHROMIUM"
 fi
 
 if [[ "$BROWSER" == "3" ]]; then
+  BROWSER_NAME="Firefox"
   TARGET_DIR="$TARGET_DIR_FIREFOX"
 fi
+
+if [[ "$BROWSER" == "4" ]]; then
+  BROWSER_NAME="Vivaldi"
+  TARGET_DIR="$TARGET_DIR_VIVALDI"
+fi
+
+echo "Installing $BROWSER_NAME host config"
 
 # Create config dir if not existing
 mkdir -p "$TARGET_DIR"
 
-if [ "$BROWSER" == "1" ] || [ "$BROWSER" == "2" ]; then
-  echo "Installing Chrome / Chromium host config"
+# Copy manifest host config file
+if [ "$BROWSER" == "1" ] || [ "$BROWSER" == "2" ] || [ "$BROWSER" == "4" ]; then
   cp "$DIR/chrome-host.json" "$TARGET_DIR/$APP_NAME.json"
 	mkdir -p "$TARGET_DIR"/../policies/managed/
-	cp "$DIR/browserpass.json" "$TARGET_DIR"/../policies/managed/"$APP_NAME.json"
+	cp "$DIR/chrome-policy.json" "$TARGET_DIR"/../policies/managed/"$APP_NAME.json"
 else
-  echo "Installing Firefox host config"
   cp "$DIR/firefox-host.json" "$TARGET_DIR_FIREFOX/$APP_NAME.json"
 fi
 
@@ -83,4 +97,4 @@ sed -i -e "s/%%replace%%/$ESCAPED_HOST_FILE/" "$TARGET_DIR/$APP_NAME.json"
 # Set permissions for the manifest so that all users can read it.
 chmod o+r "$TARGET_DIR/$APP_NAME.json"
 
-echo "Native messaging host has been installed to $TARGET_DIR."
+echo "Native messaging host for $BROWSER_NAME has been installed to $TARGET_DIR."
