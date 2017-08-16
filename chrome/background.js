30197e774f7c1ff4d316056eb555b5a283dde6d1
Iwan Timmer <irtimmer@gmail.com>
2017-08-16 17:47:01 +0200

    Use background page for filling in forms
    This commit fixes the issue of Browspass canceling the filling in when the popup loose focus.
    By using a background page Browserpass can continue to retrieve the password even when the popup is closed.
    This can happen when the password store isn't unlocked and GPG has to ask for a password.

---
+++ chrome/background.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var app = 'com.dannyvankooten.browserpass';
+
+chrome.runtime.onMessage.addListener(onMessage);
+
+// fill login form & submit
+function fillLoginForm(login) {
+	var code = `
+  (function(d) {
+	function queryAllVisible(parent, selector, form) {
+	  var result = [];
+	  var elems = parent.querySelectorAll(selector);
+	  for (var i=0; i < elems.length; i++) {
+		// Elem or its parent has a style 'display: none',
+		// or it is just too narrow to be a real field (a trap for spammers?).
+		if (elems[i].offsetWidth < 50 || elems[i].offsetHeight < 10) { continue; }
+		// Select only elements from specified form
+		if (form && form != elems[i].form) { continue; }
+
+		var style = window.getComputedStyle(elems[i]);
+		// Elem takes space on the screen, but it or its parent is hidden with a visibility style.
+		if (style.visibility == 'hidden') { continue; }
+
+		// This element is visible, will use it.
+		result.push(elems[i]);
+	  }
+	  return result;
+	}
+
+	function queryFirstVisible(parent, selector, form) {
+	  var elems = queryAllVisible(parent, selector, form);
+	  return (elems.length > 0) ? elems[0] : undefined;
+	}
+
+	function form() {
+	  var passwordField = queryFirstVisible(d, 'input[type=password]', undefined);
+	  return (passwordField && passwordField.form) ? passwordField.form : undefined;
+	}
+
+	function field(selector) {
+	  return queryFirstVisible(d, selector, form()) || document.createElement('input');
+	}
+
+	function update(el, value) {
+	  if( ! value.length ) {
+		return false;
+	  }
+
+	  el.setAttribute('value', value);
+	  el.value = value;
+
+	  var eventNames = [ 'click', 'focus', 'keypress', 'keydown', 'keyup', 'input', 'blur', 'change' ];
+	  eventNames.forEach(function(eventName) {
+		el.dispatchEvent(new Event(eventName, {"bubbles":true}));
+	  });
+	  return true;
+	}
+
+	if (typeof form() === 'undefined') {
+	  return;
+	}
+
+	update(field('input[type=password]'), ${JSON.stringify(login.p)});
+	update(field('input[type=email], input[type=text], input:first-of-type'), ${JSON.stringify(login.u)});
+
+	var password_inputs = queryAllVisible(form(), 'input[type=password]');
+	if (password_inputs.length > 1) {
+	  password_inputs[1].select();
+	} else {
+		window.requestAnimationFrame(function() {
+ field('[type=submit]').click(); 
+		});
+	 }
+  })(document);
+  `;
+	chrome.tabs.executeScript({ code: code, allFrames: true });
+}
+
+function onMessage(request, sender, sendResponse) {
+	if (request.action == "login") {
+		chrome.runtime.sendNativeMessage(app, { "action": "get", "entry": request.entry }, function(response) {
+			if( chrome.runtime.lastError) {
+				console.log(chrome.runtime.lastError);
+			}
+
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				// do not send login data to page if URL changed during search.
+				if( tabs[0].url == request.urlDuringSearch ) {
+					fillLoginForm(response, request.urlDuringSearch);
+				}
+			});
+			sendResponse();
+		});
+	}
+}
